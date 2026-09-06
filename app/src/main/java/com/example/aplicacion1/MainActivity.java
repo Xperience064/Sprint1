9d54ab13e758c36de44a3b8c7c2dcf024eb81b05
Jocelyn Soria Arroyo <jocelynsa.iti25@utsjr.edu.mx>
2026-09-06 02:39:47 -0600

    feat: implementa cierre de sesion US02

--- app/src/main/java/com/example/aplicacion1/MainActivity.java
+++ app/src/main/java/com/example/aplicacion1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aplicacion1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,73 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.aplicacion1.funcionalidad.autenticacion.controlador.ControladorCierreSesion;
+import com.example.aplicacion1.funcionalidad.autenticacion.vista.LoginActivity;
+import com.example.aplicacion1.nucleo.carrito.LimpiadorCarrito;
+import com.example.aplicacion1.nucleo.carrito.LimpiadorCarritoLocal;
+import com.example.aplicacion1.nucleo.sesion.GestorSesion;
+import com.example.aplicacion1.nucleo.sesion.GestorSesionLocal;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+
+                    Insets systemBars = insets.getInsets(
+                            WindowInsetsCompat.Type.systemBars()
+                    );
+
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom
+                    );
+
+                    return insets;
+                }
+        );
+
+        Button btnCerrarSesion =
+                findViewById(R.id.btnCerrarSesion);
+
+        GestorSesion gestorSesion =
+                new GestorSesionLocal(this);
+
+        LimpiadorCarrito limpiadorCarrito =
+                new LimpiadorCarritoLocal(this);
+
+        ControladorCierreSesion controladorCierreSesion =
+                new ControladorCierreSesion(
+                        gestorSesion,
+                        limpiadorCarrito
+                );
+
+        btnCerrarSesion.setOnClickListener(v -> {
+
+            controladorCierreSesion.cerrarSesion(() -> {
+
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        LoginActivity.class
+                );
+
+                intent.setFlags(
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                );
+
+                startActivity(intent);
+            });
         });
     }
 }
