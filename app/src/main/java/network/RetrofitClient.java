bbd0f6e11eed2abe607359a53b64c0c62ccd1e65
Jocelyn Soria Arroyo <jocelynsa.iti25@utsjr.edu.mx>
2026-09-09 20:50:06 -0600

    fix: corrige integracion y errores de compilacion

--- app/src/main/java/network/RetrofitClient.java
+++ app/src/main/java/network/RetrofitClient.java
@@ -3,17 +3,28 @@ package network;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RetrofitClient {
+public final class RetrofitClient {
+
     private static final String BASE_URL = "https://fakestoreapi.com/";
-    private static Retrofit retrofit = null;
+    private static Retrofit retrofit;
+    private static ApiService apiService;
+
+    private RetrofitClient() {
+    }
 
     public static ApiService getApiService() {
+
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
-        return retrofit.create(ApiService.class);
+
+        if (apiService == null) {
+            apiService = retrofit.create(ApiService.class);
+        }
+
+        return apiService;
     }
 }
